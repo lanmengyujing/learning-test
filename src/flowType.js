4c0d95e6ae40bb2e30588d1ac1c031742451589a
Liu Jing <jing.liu@rea-group.com>
2017-07-22 11:48:08 +0800

    add more example

--- src/flowType.js
+++ src/flowType.js
@@ -1,7 +1,32 @@
 // @flow
-function concat(a: string, b: string) {
+function concat(a: string, b: string): string {
     return a + b;
 }
 
 concat("A", "B"); // Works!
-concat(1, 2); // Error!
+
+let method = (str: string, bool?: boolean, ...nums: Array<number>): void => {
+    console.log(nums);
+};
+
+method('a', true, 2,3, [3]);
+
+function returnValue(): boolean {
+    if (Math.random() > 0.5) {
+        return true;
+    }
+    return false;
+}
+
+function testFunction(func: any) {
+    func(1, 2);     // Works.
+    func("1", "2"); // Works.
+    func({}, []);   // Works.
+}
+function testFunction(func: () => mixed) {
+}
+
+testFunction()
+
+var obj: { foo?: boolean } = {};
+obj.foo = true;

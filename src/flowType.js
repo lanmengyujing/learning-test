da52d4b70536cc14243193e5e7f6c949557f4cde
Liu Jing <jing.liu@rea-group.com>
2017-07-27 09:35:06 +0800

    add book character

--- src/flowType.js
+++ src/flowType.js
@@ -9,7 +9,7 @@ let method = (str: string, bool?: boolean, ...nums: Array<number>): void => {
     console.log(nums);
 };
 
-method('a', true, 2,3, [3]);
+method('a', true, 2, 3, [3]);
 
 function returnValue(): boolean {
     if (Math.random() > 0.5) {
@@ -30,3 +30,20 @@ testFunction()
 
 var obj: { foo?: boolean } = {};
 obj.foo = true;
+
+class BookCharacter {
+    // public alias: string;
+    public health: number;
+    private age: number;
+
+    constructor(alias, health, age) {
+        this.alias = alias;
+        this.age = age;
+        this.health = health;
+    }
+
+    getSecretIdentity() {
+        console.log(this.age)
+    }
+}
+new BookCharacter('Mike', 123, 23);
